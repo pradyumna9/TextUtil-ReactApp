34d59cacaceac49c0c6e7d406bcab248a31f47e1
Pradyumna <tech.daspk@gmail.com>
2025-03-16 22:35:25 +0000

    react-app program start

---
+++ src/contact/Contact-Header.jsx
@@ -0,0 +1,4 @@
+function ContactHeader(){
+   return   <div>Header Added</div>
+}
+export default ContactHeader
---
+++ src/contact/Contact.jsx
@@ -0,0 +1,8 @@
+function Contact(){
+   return( 
+    <div>
+        <h1>Contact Details</h1></div>
+);
+}
+
+export default Contact;
